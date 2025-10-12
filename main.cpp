5b4cd828025b3499dc63f5020b90367c669d955b
shi0001-99 <2198876433@qq.com>
2025-10-12 11:31:59 +0800

    10.12

--- main.cpp
+++ main.cpp
@@ -4,3 +4,29 @@ int main(){
     cout<<"Hello World";
     return 0;
 }
+
+
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int arr[101][101];
+    int a;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> a;
+            arr[i][j] = a;
+        }
+    }
+    for (int j = 0; j <= m; j++) {
+        for (int i = n - 1; i >= 0; i--) {
+            cout << arr[i][j]<<" ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
