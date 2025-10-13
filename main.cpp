57e5613e0a9e2940ffde180f1bee19a7b066aa71
shi0001-99 <2198876433@qq.com>
2025-10-13 23:19:17 +0800

    add 4

--- main.cpp
+++ main.cpp
@@ -158,3 +158,21 @@ int main() {
     cout << A(a, b)<<endl;
     return 0;
 }
+
+
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int is_prime(int n) {
+    if (n == 1)
+        return 0;
+    if (n == 2)
+        return 1;
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0)
+            return 0;
+    }
+    return 1;
+}
