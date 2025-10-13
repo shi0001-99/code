941511f790bc3bb0090bee8c3e7f732343c40ca4
shi0001-99 <2198876433@qq.com>
2025-10-13 22:32:05 +0800

    add 2

--- main.cpp
+++ main.cpp
@@ -114,3 +114,23 @@ int main() {
     }
     return 0;
 }
+
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int sum(int n) {
+    if (n == 1) {
+        return 1;
+    }
+    else {
+        return sum(n - 1) + n;
+    }
+}
+int main() {
+    int n;
+    cin >> n;
+    cout << sum(n);
+    return 0;
+}
