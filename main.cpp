304c227fac861c6882f7b18c269b8b4d7cad7323
shi0001-99 <2198876433@qq.com>
2025-10-13 22:27:17 +0800

    10.13

--- main.cpp
+++ main.cpp
@@ -90,3 +90,27 @@ int main() {
     cout << fixed << setprecision(3) << final;
     return 0;
 }
+
+
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int f(int n) {
+    int sum = 0;
+    for (int i = 1; i < n; i++) {
+        if (n % i == 0)
+            sum += i;
+    }
+    return sum;
+}
+int main() {
+    int n;     
+    cin >> n;     
+    for (int i = 2; i <= n; i++) { 
+        if (f(i) == i)             
+            cout << i << endl; 
+    }
+    return 0;
+}
