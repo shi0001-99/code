802316737e6996f67ba146f0e46391819d5308b4
shi0001-99 <2198876433@qq.com>
2025-10-20 23:45:12 +0800

    add 4

--- main.cpp
+++ main.cpp
@@ -778,3 +778,26 @@ int main() {
     cout << final;
     return 0;
 }
+
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int sum=0;
+    int day = 0;
+    for (int i = 1; day < n; i++) {
+        for (int j = 1; j <= i; j++) {
+            sum += i;
+            day++;
+            if (day >= n)
+                break;
+        }
+    }
+    cout << sum;
+
+    return 0;
+}
