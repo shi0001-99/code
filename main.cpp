64b4b738d009715ef67ebc29064e6a15417eace8
shi0001-99 <2198876433@qq.com>
2025-10-24 22:23:30 +0800

    add 3

--- main.cpp
+++ main.cpp
@@ -937,3 +937,35 @@ int main() {
 	cout << fixed << setprecision(2) << sum;
 	return 0;
 }
+
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+bool func(int a) {
+	if (a == 1){
+		return 0;
+	}
+	if (a == 2) {
+		return 1;
+	}
+	for (int i = 2; i < a; i++) {
+		if (a % i == 0) {
+			return 0;
+		}
+	}
+	return 1;
+}
+int main() {
+	int a;
+	cin >> a;
+	for (int i = 0; i < a; i++) {
+		int b;
+		cin >> b;
+		if (func(b)) {
+			cout << b<<" ";
+		}
+	}
+	return 0;
+}
