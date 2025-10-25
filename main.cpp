95687fdb5e95e9a2032a82843ace35fae1b485d8
shi0001-99 <2198876433@qq.com>
2025-10-25 16:53:30 +0800

    add 2

--- main.cpp
+++ main.cpp
@@ -1056,3 +1056,33 @@ int main() {
 
 	return 0;
 }
+
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+struct node {
+    int id;//学号
+    double sc1, sc2;//学业成绩和素质拓展成绩
+    int score;//学业成绩和素质拓展成绩的和
+    double final_score;//综合分数
+}a[1000];//结构体定义，由于本人习惯（懒），sc1和sc2定义成了double。
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i].id >> a[i].sc1 >> a[i].sc2;
+        a[i].score = a[i].sc1 + a[i].sc2;
+        a[i].final_score = a[i].sc1 * 0.7 + a[i].sc2 * 0.3;//计算综合分数
+    }
+    for (int i = 0; i < n; i++) {
+        if (a[i].score > 140 && a[i].final_score >= 80) {//一定看清题
+            cout << "Excellent" << endl;
+        }
+        else {
+            cout << "Not excellent" << endl;
+        }
+    }
+    return 0;
+}
